79a315b1452d71bef07c2539ddead38195d6fea8
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-04-21 10:10:21 +0900

    コメント追加

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace DistanceConverter {
     internal class Program {
+        // コマンドライン引数で指定された範囲のフィートとメートルの対応表を出力する
         static void Main(string[] args) {
 
             int start = int.Parse(args[1]);
