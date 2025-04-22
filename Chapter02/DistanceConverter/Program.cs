f5f3a7f35daba4984d507f8768cb2aceddc2db2b
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-04-22 10:25:49 +0900

    インスタンスメソッドとして利用

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -15,26 +15,20 @@
 
         // フィートからメートルへの対応表を出力
         static void PrintFeetToMeterList(int start, int end) {
+            FeetConverter converter = new FeetConverter();
             for (int feet = start; feet <= end; feet++) {
-                double meter = FeetToMeter(feet);
+                double meter = converter.ToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");
             }
         }
 
         // メートルからフィートへの対応表を出力
         static void PrintMeterToFeetList(int start, int end) {
+            FeetConverter converter = new FeetConverter();
             for (int meter = start; meter <= end; meter++) {
-                double feet = MeterToFeet(meter);
+                double feet = converter.FromMeter(meter);
                 Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
-        }
-
-        static double FeetToMeter(int feet) {
-            return feet * 0.3048;
-        }
-
-        static double MeterToFeet(int meter) {
-            return meter / 0.3048;
         }
     }
 }
