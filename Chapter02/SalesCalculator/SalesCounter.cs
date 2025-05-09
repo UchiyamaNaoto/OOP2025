e51161b71996965d18b593b8fa091326bfc30976
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-05-09 14:56:59 +0900

    メソッドの移動

--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -26,5 +26,25 @@ namespace SalesCalculator {
             }
             return dict;
         }
+
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+            //売り上げデータを入れるリストオブジェクトを生成
+            List<Sale> sales = new List<Sale>();
+            //ファイルを一気に読み込み
+            string[] lines = File.ReadAllLines(filePath);
+            //読み込んだ行数分繰り返し
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                //Saleオブジェクトを生成
+                Sale sale = new Sale() {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
     }
 }
