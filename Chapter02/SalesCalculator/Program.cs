4cb155d1d8c5cc5afbf10707a09a2df9091320c4
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-05-09 16:00:19 +0900

    暗黙の型指定を使用（var）

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(@"data\sales.csv");
-            IDictionary<string, int> amountsPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string, int> obj in amountsPerStore) {
+            var sales = new SalesCounter(@"data\sales.csv");
+            var amountsPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
         }
--- Chapter02/SalesCalculator/SalesCounter.cs
+++ Chapter02/SalesCalculator/SalesCounter.cs
@@ -16,7 +16,7 @@ namespace SalesCalculator {
 
         //店舗別売り上げを求める
         public IDictionary<string,int> GetPerStoreSales() {
-            var dict = new SortedDictionary<string, int>();
+            var dict = new Dictionary<string, int>();
             foreach (var sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName)) {
                     dict[sale.ShopName] += sale.Amount;
@@ -30,11 +30,11 @@ namespace SalesCalculator {
         //売り上げデータを読み込み、Saleオブジェクトのリストを返す
         public static IEnumerable<Sale> ReadSales(string filePath) {
             //売り上げデータを入れるリストオブジェクトを生成
-            List<Sale> sales = new List<Sale>();
+            var sales = new List<Sale>();
             //ファイルを一気に読み込み
-            string[] lines = File.ReadAllLines(filePath);
+            var lines = File.ReadAllLines(filePath);
             //読み込んだ行数分繰り返し
-            foreach (string line in lines) {
+            foreach (var line in lines) {
                 string[] items = line.Split(',');
                 //Saleオブジェクトを生成
                 Sale sale = new Sale() {
