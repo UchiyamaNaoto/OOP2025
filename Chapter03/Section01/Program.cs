a9fc4c287880776fddd29ac49a87d545e955d502
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-05-14 11:50:55 +0900

    コード3.1完成

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("カウントしたい数値：");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(Count(num));
+
+        }
+
+        static int Count(int num) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var count = 0;
+            foreach (var n in numbers) {
+                if (n == num) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
