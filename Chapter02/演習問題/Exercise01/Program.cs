91df67a54b17aaa060fc620eb31bd1032c858b97
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-05-14 11:24:00 +0900

    0514演習完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -1,24 +1,72 @@
 ﻿namespace Exercise01 {
     public class Program {
         static void Main(string[] args) {
-            //2.1.3
-            var songs = new Song[] {
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
-            };
+            //歌データを入れるリストオブジェクトを生成
+            var songs = new List<Song>();
+
+            //"***** 曲の登録 *****"を出力
+            Console.WriteLine("***** 曲の登録 *****");
+
+            //何件入力があるかわからないので無限ループ
+            while ( true ) {
+                //"曲名："を出力
+                Console.Write("曲名：");
+                //入力された曲名を取得
+                string? title = Console.ReadLine();
+
+                //endが入力されたら登録終了()
+                if (title.Equals("end",StringComparison.OrdinalIgnoreCase)) 
+                    break;
+                
+                //"アーティスト名："を出力
+                Console.Write("アーティスト名：");
+                //入力されたアーティスト名を取得
+                string? artistName = Console.ReadLine();
+
+                //"演奏時間（秒）："を出力
+                Console.Write("演奏時間（秒）：");
+                //入力された演奏時間を取得
+                int length = int.Parse(Console.ReadLine());
+
+                //Songインスタンスを生成
+                //Song song = new Song(title, artistName, length);
+                Song song = new Song() {
+                    Title = title,
+                    ArtistName = artistName,
+                    Length = length
+                };
+
+                //歌データを入れるリストオブジェクトへ登録
+                songs.Add(song);
+
+                Console.WriteLine();    //改行
+            }
+
             printSongs(songs);
         }
 
         //2.1.4
-        private static void printSongs(Song[] songs) {
+        private static void printSongs(IEnumerable<Song> songs) {
+#if false
             foreach (var song in songs) {
                 var minutes = song.Length / 60;
                 var seconds = song.Length % 60;
                 Console.WriteLine($"{song.Title}, {song.ArtistName} {minutes}:{seconds:00}");
             }
+#else
+            //TimeSpan構造体を使った場合
+            foreach (var song in songs) {
+                var timespan = TimeSpan.FromSeconds(song.Length);
+                Console.WriteLine($"{song.Title}, {song.ArtistName} {timespan.Minutes}:{timespan.Seconds:00}");
+
+            }
+            //または、以下でも可
+            //foreach (var song in songs) {
+            //    Console.WriteLine(@"{0}, {1} {2:m\:ss}",
+            //        song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            //}
+
+#endif
             Console.WriteLine();
         }
 
--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -7,18 +7,17 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     //2.1.1
     public class Song {
-        public string Title { get; private set; } = string.Empty;
-        public string ArtistName { get; private set; } = string.Empty;
-        public int Length { get; private set; }
+        public string Title { get; set; } = string.Empty;
+        public string ArtistName { get; set; } = string.Empty;
+        public int Length { get; set; }
 
         //2.1.2(コンストラクタ)
-        public Song(string title, string artistname, int length) {
-            Title = title;
-            ArtistName = artistname;
-            Length = length;
-        }
-
-
+        //public Song(string title, string artistname, int length) {
+        //    Title = title;
+        //    ArtistName = artistname;
+        //    Length = length;
+        //}
+        
 
     }
 }
