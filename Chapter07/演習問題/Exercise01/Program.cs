c24f72ea9c156f0ac7cd08506c64a38934a5a941
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-05-27 11:25:23 +0900

    第7章演習問題完成

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+
+
+            Console.WriteLine("7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("7.1.5");
+            Exercise5(numbers);
+        }
+
+        private static void Exercise1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise2(int[] numbers) {
+            foreach (var n in numbers.TakeLast(2)) {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise3(int[] numbers) {
+            var strs = numbers.Select(n => n.ToString("000"));
+            foreach (var s in strs) {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void Exercise4(int[] numbers) {
+            foreach (var n in numbers.Order().Take(3)) {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise5(int[] numbers) {
+            var count = numbers.Distinct().Count(n => n >= 10);
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ LINQ/LinqSample/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LinqSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 100);
+
+            //合計値を出力
+            Console.WriteLine(numbers.Where(n => n % 8 == 0).Sum());
+
+            //foreach (var num in numbers) {
+            //    Console.WriteLine(num);
+            //}
+            
+
+        }
+
+        static void WriteTotalMemory(string header) {
+            var totalMemory = GC.GetTotalMemory(true) / 1024.0 / 1024.0;
+            Console.WriteLine($"{header}: {totalMemory:0.0 MB}");
+        }
+    }
+}
