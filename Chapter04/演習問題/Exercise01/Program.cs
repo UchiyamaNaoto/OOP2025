244ce163abb71c7718bc0af52d53383bb40e37b9
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-05-27 09:26:21 +0900

    Chapter04完成

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -1,6 +1,9 @@
 ﻿
+using System.Diagnostics;
+
 namespace Exercise01 {
     internal class Program {
+
         static void Main(string[] args) {
             List<string> langs = [
                 "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
@@ -15,15 +18,41 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-            
+            //for文
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S'))
+                    Console.WriteLine(langs[i]);
+            }
+            Console.WriteLine("");  //改行
+
+            //foreach文
+            foreach (var lang in langs) {
+                if (lang.Contains('S'))
+                    Console.WriteLine(lang);
+            }
+            Console.WriteLine("");  //改行
+
+            //while文
+            int index = 0;
+            while (index < langs.Count) {
+                if (langs[index].Contains('S'))
+                    Console.WriteLine(langs[index]);
+                index++;
+            }
+            Console.WriteLine("");  //改行
+
+
         }
 
         private static void Exercise2(List<string> langs) {
-            
+            var selected = langs.Where(x => x.Contains('S'));
+            foreach (var lang in selected) {
+                Console.WriteLine(lang);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
-            
+            Console.WriteLine(langs.Find(x => x.Length == 10) ?? "unknown");
         }
     }
 }
