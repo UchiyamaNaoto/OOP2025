0f89368359c91216e93b89646d5e326e507c4f95
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-06-13 15:24:16 +0900

    列挙型対応

--- Chapter08/PrefCapitalLocationSystem/Program.cs
+++ Chapter08/PrefCapitalLocationSystem/Program.cs
@@ -2,6 +2,13 @@
     internal class Program {
         static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
 
+        //列挙型の定義
+        public enum SelectMenu {
+            Alldisp = 1,
+            Search = 2,
+            Exit = 9
+        }
+
         static void Main(string[] args) {
             String? pref, prefCaptalLocation;
 
@@ -34,15 +41,13 @@
             Boolean endFlag = false;    //終了フラグ（無限ループを抜け出す用）
             while (!endFlag) {
                 switch (menuDisp()) {
-                    case "1":                        //一覧出力処理
+                    case SelectMenu.Alldisp:    //一覧出力処理
                         allDisp();
                         break;
-
-                    case "2"://検索処理
+                    case SelectMenu.Search:     //検索処理
                         searchPrefCaptalLocation();
                         break;
-
-                    case "9"://無限ループを抜ける
+                    case SelectMenu.Exit:       //無限ループを抜ける
                         endFlag = true;
                         break;
                 }
@@ -50,13 +55,14 @@
         }
 
         //メニュー表示
-        private static string? menuDisp() {
+        private static SelectMenu menuDisp() {
             Console.WriteLine("\n**** メニュー ****");
             Console.WriteLine("1：一覧表示");
             Console.WriteLine("2：検索");
             Console.WriteLine("9：終了");
             Console.Write(">");
-            var menuSelect = Console.ReadLine();
+
+            var menuSelect = (SelectMenu)int.Parse( Console.ReadLine());
             return menuSelect;
         }
 
