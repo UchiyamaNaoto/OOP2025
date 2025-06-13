275cd3294a207a514a5186459cce14b6903e76b2
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-06-13 13:25:10 +0900

    8章解説前

---
+++ Chapter08/PrefCapitalLocationSystem/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace PrefCapitalLocationSystem {
+    internal class Program {
+        static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
+
+        static void Main(string[] args) {
+            String? pref, prefCaptalLocation;
+
+            //入力処理
+            Console.WriteLine("県庁所在地の登録【入力終了：Ctrl + 'Z'】");
+
+            while (true) {
+                //①都道府県の入力
+                Console.Write("都道府県:");
+                pref = Console.ReadLine();
+
+                if (pref == null) break;    //無限ループを抜ける(Ctrl + 'Z')
+
+                //県庁所在地の入力
+                Console.Write("県庁所在地:");
+                prefCaptalLocation = Console.ReadLine();
+
+                //既に都道府県が登録されているか？
+                //ヒント：ContainsKeyを使用して調べる
+
+                //登録済みなら確認して上書き処理、上書きしない場合はもう一度都道府県の入力…①へ
+                //ヒント：Console.WriteLine("上書きしますか？(Y/N)");
+
+                //*　ここに入力　*******************//
+
+
+                //県庁所在地登録処理
+
+                //*　ここに入力　*******************//
+
+
+                Console.WriteLine();//改行
+            }
+
+            Boolean endFlag = false;    //終了フラグ（無限ループを抜け出す用）
+            while (!endFlag) {
+                switch (menuDisp()) {
+                    case "1":                        //一覧出力処理
+                        allDisp();
+                        break;
+
+
+                    case "2"://検索処理
+                        searchPrefCaptalLocation();
+                        break;
+
+
+                    case "9"://無限ループを抜ける
+                        //*　ここに入力　*******************//
+                        break;
+                }
+            }
+        }
+
+        //メニュー表示
+        private static string? menuDisp() {
+            Console.WriteLine("\n**** メニュー ****");
+            Console.WriteLine("1：一覧表示");
+            Console.WriteLine("2：検索");
+            Console.WriteLine("9：終了");
+            Console.Write(">");
+            var menuSelect = Console.ReadLine();
+            return menuSelect;
+        }
+
+
+        //一覧表示処理
+        private static void allDisp() {
+
+            //*　ここに入力　*******************//
+        }
+
+        //検索処理
+        private static void searchPrefCaptalLocation() {
+            Console.Write("都道府県:");
+            String? searchPref = Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            var dict = new Dictionary<Char, int>();
+
+            foreach (var c in text.ToUpper()) {
+                // 'A' … 0x41
+                //  :       :
+                // 'Z' … 0x5a
+
+
+
+                //④アルファベットならディクショナリに登録
+                //  登録済み：valueをインクリメント
+                //  未登録：valueに1を設定
+
+
+            }
+
+
+
+
+            //⑥すべての文字が読み終わったら、アルファベット順に並び替えて出力
+
+        }
+
+        private static void Exercise2(string text) {
+            
+        }
+    }
+}
