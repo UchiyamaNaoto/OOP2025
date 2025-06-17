befc92b87ab7bb3de6c5759b73f1a3af8d8fffe5
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-06-17 10:33:16 +0900

    ８章演習問題_完成

--- Chapter08/演習問題/Exercise02/Abbreviations.cs
+++ Chapter08/演習問題/Exercise02/Abbreviations.cs
@@ -30,4 +30,18 @@ public class Abbreviations {
             }
         }
     }
+
+    //8.2.1
+    public int Count => _dict.Count;
+
+    //8.2.2
+    public bool Remove(string abb) => _dict.Remove(abb);
+
+    //8.2.4
+    public IEnumerable<KeyValuePair<string,string>> GetAll() {
+        return _dict;
+    }
+
+
+
 }
--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -8,6 +8,28 @@
             abbrs.Add("IOC", "国際オリンピック委員会");
             abbrs.Add("NPT", "核兵器不拡散条約");
 
+            // 8.2.3 (Countの呼び出し例)
+            // 上のAddメソッドで、２つのオブジェクトを追加しているので、読み込んだ単語数+2が、Countの値になる。
+            var count = abbrs.Count;
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine();
+
+            // 8.2.3 (Removeの呼び出し例)
+            if (abbrs.Remove("NPT")) {
+                Console.WriteLine(abbrs.Count);
+            }
+
+            // すでに削除してあるので、falseが返る
+            if (!abbrs.Remove("NPT")) {
+                Console.WriteLine("削除できません");
+            }
+            Console.WriteLine();
+
+            //8.2.4
+            var query = abbrs.GetAll().Where(x => x.Key.Length == 3);
+
+
+
             // Getメソッドの利用例
             var names = new[] { "WHO", "FIFA", "NPT", };
             foreach (var name in names) {
