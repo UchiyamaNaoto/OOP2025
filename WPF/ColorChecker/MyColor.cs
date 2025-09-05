f0111013289a3da25723c21c8d9419d7470af20f
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-09-05 16:02:41 +0900

    0905授業終了時

---
+++ WPF/ColorChecker/MyColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ColorChecker {
+    public struct MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; }
+        public override string ToString() {
+            return base.ToString(); //←後で使いやすいように書き換える
+        }
+    }
+}
