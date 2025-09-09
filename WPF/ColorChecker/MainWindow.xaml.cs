8ac4f8a8b4b9fd3e36c6c9e184901e113cf2c30a
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-09-09 16:01:00 +0900

    スタック登録前

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,14 +19,49 @@ namespace ColorChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        MyColor currentColor;   //現在設定している色情報//****************************
+
         public MainWindow() {
             InitializeComponent();
+            DataContext = GetColorList();
+        }
+
+        /// <summary>
+        /// すべての色を取得するメソッド
+        /// </summary>
+        /// <returns></returns>
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
         }
 
         //すべてのスライダーから呼ばれるイベントハンドラ
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
             //colorAreaの色（背景色）は、スライダーで指定したRGBの色を表示する
-            //colorArea.Background =     
+            var myColor = new MyColor { Color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value),
+                Name = string.Empty };
+            colorArea.Background = new SolidColorBrush(myColor.Color);    
+        }
+
+        private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            stockList.Items.Insert(0, currentColor);//****************************
+        }
+
+        //コンボボックスから色を選択
+        private void colorSelectComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var comboSelectMyColor = (MyColor)((ComboBox)sender).SelectedItem;
+            setSliderValue(comboSelectMyColor.Color);   //スライダーを設定
+        }
+
+        //各スライダーの値を設定する
+        private void setSliderValue(Color color) {
+            rSlider.Value = color.R;
+            gSlider.Value = color.G;
+            bSlider.Value = color.B;
         }
     }
 }
--- WPF/ColorChecker/MyColor.cs
+++ WPF/ColorChecker/MyColor.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 using System.Windows.Media;
 
 namespace ColorChecker {
+    /// <summary>
+    /// 色と色名を保持するクラス
+    /// </summary>
     public struct MyColor {
         public Color Color { get; set; }
         public string Name { get; set; }
         public override string ToString() {
-            return base.ToString(); //←後で使いやすいように書き換える
+            return Name ?? string.Format("R：{0,3}  G：{1,3}  B：{2,3}", Color.R, Color.G, Color.B);
         }
     }
 }
