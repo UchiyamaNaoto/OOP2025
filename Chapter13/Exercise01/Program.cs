1d6f3937787664f6c31a311ec260521df89d2541
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-10-27 15:32:49 +0900

    P298まで

---
+++ Chapter13/Exercise01/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            
+        }
+
+        private static void Exercise1_3() {
+            
+        }
+
+        private static void Exercise1_4() {
+            
+        }
+
+        private static void Exercise1_5() {
+            
+        }
+
+        private static void Exercise1_6() {
+            
+        }
+
+        private static void Exercise1_7() {
+        
+        }
+
+        private static void Exercise1_8() {
+            
+        }
+    }
+}
---
+++ Chapter13/Section01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var price = Library.Books
+                .Where(b => b.CategoryId == 1)
+                .Max(b => b.Price);
+            Console.WriteLine(price);
+
+            Console.WriteLine();　//改行
+
+            var book = Library.Books
+                .Where(b => b.PublishedYear >= 2021)
+                .MinBy(b => b.Price);
+            Console.WriteLine(book);
+
+            Console.WriteLine();　//改行
+
+            var average = Library.Books.Average(x => x.Price);
+            var aboves = Library.Books
+                .Where(b => b.Price > average);
+            foreach (var book1 in aboves) {
+                Console.WriteLine(book1);
+            }
+        }
+    }
+}
