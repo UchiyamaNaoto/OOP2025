b9223c853c8693545064f990cc5219df32915189
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-10-30 15:07:53 +0900

    コード14.8

--- Chapter14/Section03/Form1.cs
+++ Chapter14/Section03/Form1.cs
@@ -9,17 +9,15 @@ namespace Section03 {
 
         private async void button1_Click(object sender, EventArgs e) {
             toolStripStatusLabel1.Text = "";
-            var elapsed = await Task.Run(()=> DoLongTimeWork());
-            toolStripStatusLabel1.Text = $"{elapsed}ミリ秒";
+            await DoLongTimeWork();
+            toolStripStatusLabel1.Text = "終了";
 
         }
-        //戻り値のある同期メソッド
-        private long DoLongTimeWork() {
-            var sw = Stopwatch.StartNew();
-
-            System.Threading.Thread.Sleep(5000);
-            sw.Stop();
-            return sw.ElapsedMilliseconds;
+        //非同期メソッド
+        private async Task DoLongTimeWork() {
+            await Task.Run(() => {
+                System.Threading.Thread.Sleep(5000);
+            });
         }
     }
 }
