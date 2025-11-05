d42343b75855ca123384b0e3acd9b1a74304ff39
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-11-05 10:56:34 +0900

    演習14-1完成

--- Chapter14/演習問題/Exercise01_Console/Program.cs
+++ Chapter14/演習問題/Exercise01_Console/Program.cs
@@ -1,27 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 internal class Program {
-    static void Main(string[] args) {
-        string text = TextReaderSample.ReadText("吾輩は猫である.txt");
+    static async Task Main(string[] args) {
+        string text = await TextReaderSample.ReadText("走れメロス.txt");
         Console.WriteLine(text);
 
         Console.WriteLine("End!");
     }
 
+    //非同期のファイル読み込み処理
     static class TextReaderSample {
-        public static string ReadText(string filePath) {
+        public static async Task<string> ReadText(string filePath) {
             var sb = new StringBuilder();
 
             // using を使って自動的にファイルを閉じる
             using (var sr = new StreamReader(filePath, Encoding.UTF8)) {
                 while (!sr.EndOfStream) {
-                    string? line = sr.ReadLine();  // 1行読み取り
+                    string? line = await sr.ReadLineAsync();  // 1行読み取り
                     sb.AppendLine(line);
                 }
             }
-
             return sb.ToString();
         }
     }
--- Chapter14/演習問題/Exercise01_Wpf/MainWindow.xaml.cs
+++ Chapter14/演習問題/Exercise01_Wpf/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,6 +18,27 @@ namespace Exercise01_Wpf {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() is true) {
+                TextArea.Text = await TextReaderSample.ReadTextAsync(ofd.FileName);
+            }
+
+        }
+    }
+    //非同期のファイル読み込み処理
+    static class TextReaderSample {
+        public static async Task<string> ReadTextAsync(string filePath) {
+            var sb = new StringBuilder();
+            var sr = new StreamReader(filePath);
+            while (!sr.EndOfStream) {
+                var line = await sr.ReadLineAsync();
+                sb.AppendLine(line);
+                await Task.Delay(10);    //わざと遅延させる
+            }
+            return sb.ToString();
         }
     }
 }
