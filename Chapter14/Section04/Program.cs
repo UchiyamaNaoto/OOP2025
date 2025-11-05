ead55ea551ac5ea23baabf761a118071bbc7698b
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-11-05 11:16:38 +0900

    コード14-13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            HttpClient hc = new HttpClient();
+            await GetHtmlExample( hc );
+        }
+
+        static async Task GetHtmlExample(HttpClient httpClient) {
+            var url = "https://www.yahoo.co.jp/";
+            var text = await httpClient.GetStringAsync(url);
+            Console.WriteLine(text);
+        }
+
+    }
+}
