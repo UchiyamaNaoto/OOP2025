7c4fee8e5e7d535949949f03697ee84591b289d0
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-11-06 15:17:21 +0900

    並列処理ありとなしの比較サンプル

---
+++ Chapter14/Section05/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("並列処理あり");
+
+            Parallel.For(0, 100, i => {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(100);
+                Console.WriteLine($"処理{i}終了");
+            });
+
+            Console.WriteLine("並列処理なし");
+            
+            for (int i = 0; i < 100; i++) {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(100);
+                Console.WriteLine($"処理{i}終了");
+            }
+        }
+    }
+}
