339cbfa0a20b77f8002285e5e37a25e377bcc2f0
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-11-12 11:31:45 +0900

    P344完成

---
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -0,0 +1,9 @@
+﻿using TextFileProcessor;
+
+namespace LineCounter {
+    internal class Program {
+        static void Main(string[] args) {
+            TextProcessor.Run<LineCounterProcessor>(args[0]);
+        }
+    }
+}
