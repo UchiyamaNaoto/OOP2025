de5bd12c5746e719d278f81c2a908d6b618448a4
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-11-12 10:12:43 +0900

    継承を使ったポリモーフィズム

---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<GreetingBase> list = [
+                new GreetingMorning(),
+                new GreetingAfternoon(),
+                new GreetingEvening(),
+                ];
+
+            foreach (var obj in list) {
+                string msg = obj.GetMessage();
+                Console.WriteLine(msg);
+                
+                
+            }
+        }
+    }
+
+    class GreetingMorning : GreetingBase {
+        public override string GetMessage() => "おはよう";
+    }
+
+    class GreetingAfternoon: GreetingBase {
+        public override string GetMessage() => "こんにちは";
+    }
+
+    class GreetingEvening : GreetingBase {
+        public  override string GetMessage() => "こんばんは";
+    }
+
+}
