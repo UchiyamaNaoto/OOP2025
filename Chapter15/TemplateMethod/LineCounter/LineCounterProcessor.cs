f55562e45534ebcf720c9b1c85b9b8e567d41ad8
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-11-12 12:10:59 +0900

    単語を入力してカウントするプログラム

--- Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -8,12 +8,18 @@ using TextFileProcessor;
 namespace LineCounter {
     internal class LineCounterProcessor : TextProcessor{
         private int _count = 0;
+        private string _searchWord = "";
 
-        protected override void Initialize(string fname) => _count = 0;
+        protected override void Initialize(string fname) {
+            _count = 0;
+            Console.Write("カウントしたい単語：");
+            _searchWord = Console.ReadLine();
+        }
+        protected override void Execute(string line) {
+            _count += line.Split(_searchWord).Length - 1;
+        }
 
-        protected override void Execute(string line) => _count++;
-
-        protected override void Terminate() => Console.WriteLine("{0} 行", _count);
+        protected override void Terminate() => Console.WriteLine("{0}の個数：{1}", _searchWord, _count);
             
     }
 }
