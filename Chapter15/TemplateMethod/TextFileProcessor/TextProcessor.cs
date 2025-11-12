76b1aaebd297ff9aa9cf9fbb190aa6b965d0b11f
UchiyamaNaoto <n-uchiyama@ojs.ac.jp>
2025-11-12 11:08:49 +0900

    TextFileProcessorのフレームワーク完成

---
+++ Chapter15/TemplateMethod/TextFileProcessor/TextProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessor {
+    public abstract class TextProcessor {
+        public static void Run<T>(string fileName) where T : TextProcessor, new() {
+            var self = new T();
+            self.Process(fileName);
+        }
+
+        private void Process(string fileName) {
+            Initialize(fileName);
+            var lines = File.ReadLines(fileName);
+            foreach (var line in lines) {
+                Execute(line);
+            }
+            Terminate();
+        }
+
+        protected virtual void Initialize(string fname) { }
+        protected virtual void Execute(string line) { }
+        protected virtual void Terminate() { }
+    }
+}
